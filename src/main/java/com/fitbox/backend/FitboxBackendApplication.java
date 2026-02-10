5f03a4e37c419868468a1fccbb26b7d6fad61ed9
Umut Ekin KARASLAN <senin@emailin.com>
2026-02-10 12:45:11 +0300

    chore: initialize Spring Boot backend project

---
+++ src/main/java/com/fitbox/backend/FitboxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitbox.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitboxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitboxBackendApplication.class, args);
+	}
+
+}
